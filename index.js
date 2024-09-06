8fe626d76e5e4082b746d435c800dc1d249b97b0
Silindokuhle Ngubane <silindokuhlengubane0@gmail.com>
2024-09-06 21:02:26 +0200

    APIs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
+        
     })
     .catch(err => {
         // Use a default background image/author
